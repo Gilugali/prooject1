0ab216e781678a88d1a3e27864a6c1f1995660c4
mugishajuste <mugishajuste1@gmail.com>
2022-08-18 22:50:49 +0200

    just a little changedd

--- node.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const server = http.createServer((req, res) =>{
-    res.end(end)('hello');
-});
-server.listen(200, () =>{
-    console.log('server is running.,..');
-});
